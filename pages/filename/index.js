a868a922700b102bda4fd9ff8e78966a0f81ab1a
ROBOT-RIAD <riadhosain420@gmail.com>
2024-10-12 14:51:13 +0600

    add dynamic roout

--- pages/filename/index.js
+++ pages/filename/index.js
@@ -2,6 +2,7 @@ import User from "@/components/navbar/User";
 export const getStaticProps = async () =>{
     const res= await fetch("https://jsonplaceholder.typicode.com/users");
     const data= await res.json();
+    
     return {
         props: {users : data}
     }
